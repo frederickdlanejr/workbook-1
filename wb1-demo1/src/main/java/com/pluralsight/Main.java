01bd72e3d9c09dc17776bd363f500fefe364d6ae
frederickdlanejr <frederickdlanejr@gmail.com>
2025-04-14 11:50:53 -0400

    Added code

--- wb1-demo1/src/main/java/com/pluralsight/Main.java
+++ wb1-demo1/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,31 @@
 package com.pluralsight;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        String word1 = "Hello";
+
+        String word2 = "World";
+
+        String greeting;
+
+        greeting = word1 + " " + word2 + "!";
+
+
+        System.out.println(greeting);
+
+///////////////////////////////////////////////
+
+long aLong;
+int bInt;
+
+float taxRate = 0.15f;
+aLong = 777777777_000_000_000L;
+bInt = (int) aLong;
+
+System.out.print(bInt);
+
     }
 }
